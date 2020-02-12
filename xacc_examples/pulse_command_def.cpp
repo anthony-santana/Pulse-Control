5812a7da7cd7711362fed04e3119c76a0837417f
Thien Nguyen <nguyentm@ornl.gov>
2020-02-12 15:29:30 -0500

    Refactor an obsolete example
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- xacc_examples/pulse_command_def.cpp
+++ xacc_examples/pulse_command_def.cpp
@@ -1,29 +1,44 @@
 #include "xacc.hpp"
+#include <cassert>
 #include "Pulse.hpp"
 #include "xacc_service.hpp"
 
 
 // ============================================================================
-// This example demonstrate the use of commnand level IR (in Pulse mode):
-// i.e. a command (e.g. X, CX) is translated to a sequence of pulses
-// and simulate dynamically. 
+// This example demonstrates the backend configs loading, e.g. from IBM JSON file
+// i.e., the Hamiltonian will be parsed directly from the Json file.
 // ============================================================================
 int main (int argc, char** argv) {
 	// Initialize the XACC Framework
 	xacc::Initialize(argc, argv);
-    // Use the mock 1Q backend
-    const std::string backendName = "Fake1Q";
-   
-    auto quaC = xacc::getAccelerator("QuaC", { std::make_pair("backend", backendName), std::make_pair("config-json-path", "/home/cades/dev/xacc/quantum/gate/ir/tests/files/test_backends.json") });    
+    // IBM 20-qubit device Poughkeepsie backend JSON
+    const std::string jsonConfigFile = std::string(GATEIR_TEST_FILE_DIR) + "/test_backends.json";
+    
+    // By providing a full backend config, the XACC-Quac accelerator will:
+    // (1) Parse the system Hamiltonian from the Json
+    // (2) Parse all the pulses that are defined (by samples) and all the so-called 'cmd-def''s
+    // which are essentially sequence of pulses to realize a gate.
+    auto quaC = xacc::getAccelerator("QuaC", 
+        { std::make_pair("config-json-path", jsonConfigFile) });    
 
-    // Create a simple pulse program using IR
-    auto provider = xacc::getIRProvider("quantum");
-    auto program = provider->createComposite("test_program");
-    auto x0 = xacc::getContributedService<xacc::Instruction>("pulse::x_0");
-    program->addInstructions({ x0 });   
-
-    auto qubitReg = xacc::qalloc(1);    
-    quaC->execute(qubitReg, program);
+    // At this point, the whole pulse library has been imported.
+    // For example, we can retrieve the pulse definition for a X(q[0]) gate for this device:
+    auto x0 = xacc::ir::asComposite(xacc::getContributedService<xacc::Instruction>("pulse::x_0"));
+    // For the Poughkeepsie device, X gate is just a single PI pulse named "Xp_d0_ddeb" (specific for qubit #0, i.e., d0 channel)
+    assert(x0->nInstructions() == 1);
+    assert(x0->getInstruction(0)->name() == "Xp_d0_ddeb");
+    // Print out the definition
+    std::cout << "X(q[0]): \n" << x0->toString() << "\n";
+    
+    // Similarly, CNOT (CX) gate is also defined in terms of pulses (and frame changes) 
+    // which we have loaded from the JSON file. 
+    // e.g. CNOT(q[0], q[1])
+    auto cx01 = xacc::ir::asComposite(xacc::getContributedService<xacc::Instruction>("pulse::cx_0_1"));
+    // For the Poughkeepsie device, CX gate is constructed from 10 commands (7 pulses and 3 frame changes)
+    assert(cx01->nInstructions() == 10);
+    
+    // Print out the definition
+    std::cout << "CNOT(q[0], q[1]): \n" << cx01->toString() << "\n";
     
     // Finalize the XACC Framework
 	xacc::Finalize();
