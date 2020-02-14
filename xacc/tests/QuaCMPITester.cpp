6a44c4f831ac11496fd1f7d67174bb64f77a0c1a
Thien Nguyen <nguyentm@ornl.gov>
2020-02-14 11:11:39 -0500

    Add an example and a test case of using pulses and cmd-defs from IBM backend JSON
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- xacc/tests/QuaCMPITester.cpp
+++ xacc/tests/QuaCMPITester.cpp
@@ -4,6 +4,7 @@
 #include "PulseSystemModel.hpp"
 #include "PulseGen.hpp"
 #include <chrono>
+#include <random>
 
 // This test will also validate our Hamiltonian parsing (SUM terms)
 // Run the 10-qubit Heisenberg spin chain simulation with MPI
@@ -115,6 +116,125 @@ TEST(QuaCMPITester, checkSpinChain)
     {
         EXPECT_TRUE(data < 0.25);
     }
+}
+
+// Test the X gate for IBM backend
+// To reduce test time, just use 5 qubits of the 20 qubits
+TEST(QuaCMPITester, testXGateIBMLib) 
+{
+    const std::string hamiltonianJson = R"(
+    {
+        "description": "Qubits are modelled as a two level system.\n",
+        "h_str": ["_SUM[i,0,4,wq{i}/2*Z{i}]", "_SUM[i,0,4,omegad{i}*X{i}||D{i}]"],
+        "osc": {},
+        "qub": {
+            "0": 2,
+            "1": 2,
+            "2": 2,
+            "3": 2,
+            "4": 2
+        },
+        "vars": {
+            "omegad0": 1.25,
+            "omegad1": 0.97, 
+            "omegad2": 2.5,
+            "omegad3": 1.05,
+            "omegad4": 0.845,
+            "wq0": 30.91270129264568,
+            "wq1": 30.36010168900955,
+            "wq2": 31.041771660759178,
+            "wq3": 28.36701429077905,
+            "wq4": 29.298278199939336
+        }
+    })";
+    
+    std::vector<double> loFreqs {
+        30.91270129264568,
+        30.36010168900955,
+        31.041771660759178,
+        28.36701429077905,
+        29.298278199939336
+    };
+
+    // We don't use U channels (cross-resonance) at the moment.
+    std::vector<double> loFreqs_uChannels(20, 0.0);
+
+    for (auto& freq : loFreqs)
+    {
+        freq = freq/(2*M_PI);
+    }
+
+    auto systemModel = std::make_shared<QuaC::PulseSystemModel>();
+    const bool loadOk = systemModel->loadHamiltonianJson(hamiltonianJson);
+    assert(loadOk);
+    
+    BackendChannelConfigs channelConfigs;
+    channelConfigs.dt = 3.5555555555555554;
+    channelConfigs.loFregs_dChannels = loFreqs;
+    channelConfigs.loFregs_uChannels = loFreqs_uChannels;
+    systemModel->setChannelConfigs(channelConfigs);    
+    const int NB_SHOTS = 10000;
+    const int NB_QUBITS = 5;
+
+    // Random select a qubit to run
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    // Select a qubit from 0->4
+    std::uniform_int_distribution<std::mt19937::result_type> dist(0, NB_QUBITS - 1);
+    const int qubitIdx = dist(rng);
+    std::cout << "Perform X[q" << qubitIdx << "]\n";
+    
+    // Template for the XASM (randomly assign a qubit to the X gate)
+    const std::string xasmTmpl = 
+        R"(__qpu__ void testX(qbit q) {
+            X(q[%%Idx%%%]);  
+            Measure(q[0]);
+            Measure(q[1]);
+            Measure(q[2]);
+            Measure(q[3]);
+            Measure(q[4]);
+        })";
+
+    // Randomly pick a qubit
+    const std::string qubitIdxPlaceholder = "%%Idx%%%";
+    std::string randomXasm = xasmTmpl;
+    randomXasm.replace(randomXasm.find(qubitIdxPlaceholder), qubitIdxPlaceholder.length(), std::to_string(qubitIdx));
+
+    auto quaC = xacc::getAccelerator("QuaC", { std::make_pair("system-model", systemModel), 
+                                                std::make_pair("execution-mode", "MPI"),
+                                                std::make_pair("shots", NB_SHOTS) });
+    
+    // Load cmd-def from IBM Json
+    const std::string jsonConfigFile = std::string(GATEIR_TEST_FILE_DIR) + "/test_backends.json";
+    std::ifstream backendFile(jsonConfigFile);
+    std::string jjson((std::istreambuf_iterator<char>(backendFile)), std::istreambuf_iterator<char>());
+    quaC->contributeInstructions(jjson);     
+   
+    auto qubitReg = xacc::qalloc(NB_QUBITS);
+    auto provider = xacc::getIRProvider("quantum");
+    auto xasmCompiler = xacc::getCompiler("xasm");
+    
+    // Run the XASM circuit (randomly pick a qubit for X gate)
+    auto ir = xasmCompiler->compile(randomXasm, quaC);    
+    auto program = ir->getComposite("testX"); 
+    quaC->execute(qubitReg, program);
+
+    // Construct the expected bit string: '1' for the qubit that was picked, 
+    // '0' for all others.
+    std::string expectedBitString;
+    for (int i = 0; i < NB_QUBITS; ++i)
+    {
+        expectedBitString.push_back(i == qubitIdx ? '1' : '0');
+    }
+
+    const double probResult = qubitReg->computeMeasurementProbability(expectedBitString);
+    
+    // The probability of the expected bitstring should be close to 1.0
+    EXPECT_NEAR(probResult, 1.0, 0.1);
+    // Debug:
+    qubitReg->print();
+    // Finalize the XACC Framework
+	xacc::Finalize();
 }
 
 
--- xacc_examples/hamiltonian_json_input.cpp
+++ xacc_examples/hamiltonian_json_input.cpp
@@ -1,65 +1,136 @@
-
 #include "xacc.hpp"
+#include <cassert>
 #include "Pulse.hpp"
+#include "xacc_service.hpp"
+#include "PulseSystemModel.hpp"
 
+// ============================================================================
+// Using cmd-def's of single-qubit gates from IBM Poughkeepsie backend 
+// ============================================================================
 int main (int argc, char** argv) {
 	// Initialize the XACC Framework
 	xacc::Initialize(argc, argv);
     
-    // Use the mock 2Q backend
-    const std::string backendName = "Fake2Q";
-   
-    // An example two-qubit Hamiltonian (adapted from Qiskit AER pulse_sim)
+    // NOTES: 
+    // (1) Although we will load all cmd-def's from the IBM Json,
+    // we need to *modify* the Hamiltonian to make it work.
+    // This is because all the driving terms in the IBM Json have ZERO coefficients
+    // which doesn't make any sense (we cannot drive the qubits).
+    // For this example, we use a trial-and-error approach to set these coefficients
+    // based on the cmd-def for X gates.
+    // i.e. we do X(q[i]) which will look up the cmd-def and associated pulses from the 
+    // IBM Json, drive the qubits with those pulses, and tune the coefficients to 
+    // make the X gate.  
+    // Hence, the omegad0, omegad1, etc. in the below JSON are approximate that we 
+    // have calibrated (very roughly, upto 10% error rate for X gates) to match
+    // those pulses defined in the JSON files.
+    // (2) We only use 10-qubit for demonstration, 
+    //  i.e. we will only ever use a subset of cmd-defs defined.
     const std::string hamiltonianJson = R"(
-        {
-            "description": "Two-qubit Hamiltonian.",
-            "h_latex": "",
-            "h_str": ["pi*(2*v0-alpha0)*O0", "pi*alpha0*O0*O0", "2*pi*r*X0||D0", "2*pi*r*X0||U1", "2*pi*r*X1||U0", "pi*(2*v1-alpha1)*O1", "pi*alpha1*O1*O1", "2*pi*r*X0||D1", "2*pi*j*SP0*SM1", "2*pi*j*SM0*SP1"],
-            "osc": {},
-            "qub": {
-                "0": 2,
-                "1": 2
-            },
-            "vars": {
-                "v0": 5.00, 
-                "v1": 5.1, 
-                "j": 0.01, 
-                "r": 0.02, 
-                "alpha0": -0.33, 
-                "alpha1": -0.33
-            }
+    {
+        "description": "Qubits are modelled as a two level system.\n",
+        "h_str": ["_SUM[i,0,9,wq{i}/2*Z{i}]", "_SUM[i,0,9,omegad{i}*X{i}||D{i}]"],
+        "osc": {},
+        "qub": {
+            "0": 2,
+            "1": 2,
+            "2": 2,
+            "3": 2,
+            "4": 2,
+            "5": 2,
+            "6": 2,
+            "7": 2,
+            "8": 2,
+            "9": 2
+        },
+        "vars": {
+            "omegad0": 1.25,
+            "omegad1": 0.97, 
+            "omegad2": 2.5,
+            "omegad3": 1.05,
+            "omegad4": 0.845,
+            "omegad5": 1.25,
+            "omegad6": 1.25,
+            "omegad7": 0.97,
+            "omegad8": 1.33,
+            "omegad9": 1.45,
+            "wq0": 30.91270129264568,
+            "wq1": 30.36010168900955,
+            "wq2": 31.041771660759178,
+            "wq3": 28.36701429077905,
+            "wq4": 29.298278199939336,
+            "wq5": 31.14757431485366,
+            "wq6": 31.387741224914162,
+            "wq7": 30.232349262897678,
+            "wq8": 31.502130591468386,
+            "wq9": 31.769632280927663
         }
-    )";
+    })";
+    
+    std::vector<double> loFreqs {
+        30.91270129264568,
+        30.36010168900955,
+        31.041771660759178,
+        28.36701429077905,
+        29.298278199939336,
+        31.14757431485366,
+        31.387741224914162,
+        30.232349262897678,
+        31.502130591468386,
+        31.769632280927663
+    };
 
-    auto quaC = xacc::getAccelerator("QuaC", { std::make_pair("backend", backendName), std::make_pair("hamiltonian", hamiltonianJson) });    
+    // We don't use U channels (cross-resonance) at the moment.
+    std::vector<double> loFreqs_uChannels(20, 0.0);
 
-    auto qubitReg = xacc::qalloc(2);    
+    for (auto& freq : loFreqs)
+    {
+        freq = freq/(2*M_PI);
+    }
 
-
-    auto randomPulse = std::make_shared<xacc::quantum::Pulse>("myPulse", "d0");
-    std::vector<std::vector<double>> samples = {
-        {0.0, 0.0}, {0.013434, 0.0}, {0.058597, 0.0}, {0.146446, 0.0},  
-        {0.229930, 0.0}, {0.229930, 0.0}, {0.1464464, 0.0},  
-        {0.058597, 0.0}, {0.013434, 0.0}, {0.0, 0.0}, {0.0, 0.0},  
-        {0.009035, 0.0}, {0.039411, 0.0}, {0.098498, 0.0},  
-        {0.154648, 0.0}, {0.154648, 0.0}, {0.098498, 0.0},  
-        {0.039411, 0.0}, {0.009035, 0.0}, {0.0, 0.0}, {0.0, 0.0},  
-        {0.009035, 0.0}, {0.039411, 0.0}, {0.098498, 0.0},  
-        {0.154648, 0.0}, {0.154648, 0.0}, {0.098498, 0.0},  
-        {0.039411, 0.0}, {0.009035, 0.0}, {0.0, 0.0}, {0.0, 0.0}, 
-        {0.013434, 0.0}, {0.058597, 0.0}, {0.146446, 0.0},  
-        {0.229930, 0.0}, {0.229930, 0.0}, {0.146446, 0.0},  
-        {0.058597, 0.0}, {0.013434, 0.0}, {0.0, 0.0}
-    };   
-
-    randomPulse->setSamples(samples);
+    auto systemModel = std::make_shared<QuaC::PulseSystemModel>();
+    const bool loadOk = systemModel->loadHamiltonianJson(hamiltonianJson);
+    assert(loadOk);
+    
+    BackendChannelConfigs channelConfigs;
+    channelConfigs.dt = 3.5555555555555554;
+    channelConfigs.loFregs_dChannels = loFreqs;
+    channelConfigs.loFregs_uChannels = loFreqs_uChannels;
+    systemModel->setChannelConfigs(channelConfigs);    
+    
+    const int NB_SHOTS = 10000;
+    auto quaC = xacc::getAccelerator("QuaC", { std::make_pair("system-model", systemModel), 
+                                                std::make_pair("shots", NB_SHOTS) });
+    
+    
+    // Contribute all cmd-defs in the backend Json as XACC instruction.
+    // This will activate Gates -> Pulses decomposition when simulating the circuit.
+    const std::string jsonConfigFile = std::string(GATEIR_TEST_FILE_DIR) + "/test_backends.json";
+    std::ifstream backendFile(jsonConfigFile);
+    std::string jjson((std::istreambuf_iterator<char>(backendFile)), std::istreambuf_iterator<char>());
+    quaC->contributeInstructions(jjson);     
+    
+    // Note: this may take a long time to run.
+    auto qubitReg = xacc::qalloc(10);
     auto provider = xacc::getIRProvider("quantum");
-    auto compositeInst = provider->createComposite("test_pulse");
-    compositeInst->addInstruction(randomPulse);
-
+    auto xasmCompiler = xacc::getCompiler("xasm");
+    
+    // Hadamard by pulses:
+    // Note: we only calibrate the Hamiltonian by the X gate (pulse named Xp_....)
+    // we expect the calibrated Hamiltonian coefficients should also work reasonably well
+    // for other pulses defined in this backend library.
+    // e.g the Hadamard gate will be decomposed into X90p... and X90m... pulses along with a few frame changes.  
+    auto ir = xasmCompiler->compile(R"(__qpu__ void test(qbit q) {
+      H(q[0]);
+      Measure(q[0]);
+    })", quaC);
+    
+    auto program = ir->getComposite("test");
+  
     // Run the Pulse simulation with the Hamiltonian provided
-    quaC->execute(qubitReg, compositeInst);
+    quaC->execute(qubitReg, program);
 
+    qubitReg->print();
     // Finalize the XACC Framework
 	xacc::Finalize();
 
