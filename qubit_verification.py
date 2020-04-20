88d91383b7b2f78d050a4be3e20b030bf843a752
A.M. Santana <prostoc11@me.com>
2020-04-20 19:58:11 +0000

    Adding some files to save my progress

--- qubit_verification.py
+++ qubit_verification.py
@@ -47,32 +47,36 @@ channelConfig.loFregs_dChannels = [1.0]
 model.setChannelConfigs(channelConfig)
 
 delta = -0.1
-fidelity = np.zeros(100)
-deltas = np.zeros(100)
-for i in range(100):
-    noise_signal = (1 + delta)
-    weights = np.array([-5., 1.65775359, 0.31740352, -3.74176682])
-    pulseData = (weights * Slepians).sum(axis=1) * noise_signal
-    # Add that slepian pulse instruction to XACC
-    pulseName = 'Slepian' 
-    print(pulseName)
-    xacc.addPulse(pulseName, pulseData)   
-    q = xacc.qalloc(1)
-    # Create the quantum program that contains the slepian pulse
-    # and the drive channel (D0) is set on the instruction
-    provider = xacc.getIRProvider('quantum')
-    prog = provider.createComposite('pulse')
-    slepianPulse = provider.createInstruction(pulseName, [0])
-    slepianPulse.setChannel('d0')
-    prog.addInstruction(slepianPulse)
-    # Measure Q0 (using the number of shots that was specified above)
-    prog.addInstruction(xacc.gate.create("Measure", [0]))
-    # Run the simulation
-    qpu.execute(q, prog)
-    resultProb = q.computeMeasurementProbability('1')
-    fidelity[i] = resultProb
-    print(resultProb)
-    delta += 0.002
+# fidelity = np.zeros((20, 50))
+# deltas = np.zeros(20)
+# for i in range(20):
+fidelity = np.zeros((20, 50))
+deltas = np.zeros(20)
+for i in range(20):
+    for j in range(50):
+        noise_signal = (1 + delta)
+        weights = np.array([-5., 1.65775359, 0.31740352, -3.74176682])
+        pulseData = (weights * Slepians).sum(axis=1) * noise_signal
+        # Add that slepian pulse instruction to XACC
+        pulseName = 'Slepian' 
+        print(pulseName)
+        xacc.addPulse(pulseName, pulseData)   
+        q = xacc.qalloc(1)
+        # Create the quantum program that contains the slepian pulse
+        # and the drive channel (D0) is set on the instruction
+        provider = xacc.getIRProvider('quantum')
+        prog = provider.createComposite('pulse')
+        slepianPulse = provider.createInstruction(pulseName, [0])
+        slepianPulse.setChannel('d0')
+        prog.addInstruction(slepianPulse)
+        # Measure Q0 (using the number of shots that was specified above)
+        prog.addInstruction(xacc.gate.create("Measure", [0]))
+        # Run the simulation
+        qpu.execute(q, prog)
+        resultProb = q.computeMeasurementProbability('1')
+        fidelity[i,j] = resultProb
+        print(resultProb)
+    delta += 0.01
     deltas[i] = delta
 np.savetxt('fidelity_delta.csv', fidelity, delimiter=',')
 np.savetxt('deltas.csv', deltas, delimiter=',')
